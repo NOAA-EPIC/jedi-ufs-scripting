5a71afda5a8e096540a891c84b82db32af1a8d28
Ben Koziol <bkoziol@redlineperf.com>
2025-07-09 11:15:52 -0500

    attempt build/test

--- work/benkozi-work.sh
+++ work/benkozi-work.sh
@@ -2,6 +2,7 @@
 
 set -xue
 
+export JU_CLONE_DIR=/scratch3/NCEPDEV/stmp/Benjamin.Koziol/sandbox/NOAA-EPIC/test/nest/me
 export SLURM_ACCOUNT=epic
 
 cd /scratch3/NCEPDEV/stmp/Benjamin.Koziol/sandbox/NOAA-EPIC/jedi-ufs-scripting
