131c729d5f54ab0c85e66766ac46ec6abff15d42
Ben Koziol <bkoziol@redlineperf.com>
2025-07-09 11:50:34 -0500

    move code around

--- in-line/ij-do-all.sh
+++ in-line/ij-do-all.sh
@@ -9,8 +9,6 @@ fi
 
 source ./util/ij-env.sh
 
-exit 1
-
-source ./ij-clone.sh 2>&1 | tee out.ij-clone."${JU_SUFFIX}"
+source ./ij-clone.sh 2>&1 | tee out.ij-clone."${JU_SUFFIX}" || exit 1
 source ./ij-build.sh 2>&1 | tee out.ij-build."${JU_SUFFIX}"
 source ./ij-test.sh 2>&1 | tee out.ij-test."${JU_SUFFIX}"
