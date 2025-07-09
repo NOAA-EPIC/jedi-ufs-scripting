66ffefd583fadde9bd6cea9519ff4b058c9e265a
Ben Koziol <bkoziol@redlineperf.com>
2025-07-09 11:29:57 -0500

    attempt build/test

--- ju-do-all.sh
+++ ju-do-all.sh
@@ -7,6 +7,8 @@ if [[ ! -v SLURM_ACCOUNT ]]; then
   exit 1
 fi
 
+source ./util/ju-env.sh
+
 bash ./ju-clone.sh 2>&1 | tee out.clone-jedi-ufs."${JU_SUFFIX}"
 bash ./ju-build.sh 2>&1 | tee out.build-jedi-ufs."${JU_SUFFIX}"
 bash ./ju-test.sh 2>&1 | tee out.test-jedi-ufs."${JU_SUFFIX}"
