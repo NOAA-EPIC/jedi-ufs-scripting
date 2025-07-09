ea6d73045cde3f82fcee94f820add1fb2e5d2866
Ben Koziol <bkoziol@redlineperf.com>
2025-07-09 11:37:59 -0500

    move code around

--- in-line/work/benkozi-work.sh
+++ in-line/work/benkozi-work.sh
@@ -5,6 +5,6 @@ set -xue
 export JU_CLONE_DIR=/scratch3/NCEPDEV/stmp/Benjamin.Koziol/sandbox/NOAA-EPIC/tmp/global-workflow.test.20250709-1127
 export SLURM_ACCOUNT=epic
 
-cd /scratch3/NCEPDEV/stmp/Benjamin.Koziol/sandbox/NOAA-EPIC/jedi-ufs-scripting
+cd /scratch3/NCEPDEV/stmp/Benjamin.Koziol/sandbox/NOAA-EPIC/jedi-ufs-scripting/in-line
 
 bash ./ij-do-all.sh
