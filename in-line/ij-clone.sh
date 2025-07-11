1d6abcffa14580e5ad8ef97d7ee59f0a465c19ae
Ben Koziol <bkoziol@redlineperf.com>
2025-07-11 10:59:03 -0500

    build experiment

--- in-line/ij-clone.sh
+++ in-line/ij-clone.sh
@@ -13,7 +13,7 @@ git clone -b feature/JEDI-inline --recursive --jobs 8 https://github.com/NOAA-EP
 
 # Set the appropriate WM hash to work with spack-stack 1.6.0 -----
 
-sed -i "s/\(set(UFS_TAG[^(]*\)\"develop\"/\1\"${_IJ_UFS_TAG}\"/" "${_IJ_GDAS_DIR}"/bundle/CMakeLists.txt
+sed -i "s/\(set(UFS_TAG[^(]*\)\"develop\"/\1\"${IJ_UFS_TAG}\"/" "${_IJ_GDAS_DIR}"/bundle/CMakeLists.txt
 
 # Add the physics suite needed for the in-line experiment -----
 
--- in-line/work/benkozi-work.sh
+++ in-line/work/benkozi-work.sh
@@ -2,10 +2,10 @@
 
 set -xue -o pipefail
 
-export IJ_SUFFIX=20250709-1208
-export IJ_CLONE_DIR=/scratch3/NCEPDEV/stmp/Benjamin.Koziol/sandbox/NOAA-EPIC/tmp/global-workflow.test.${IJ_SUFFIX}
+export IJ_SUFFIX=20250711-1056
+export IJ_CLONE_DIR=/scratch3/NCEPDEV/stmp/Benjamin.Koziol/inline-jedi-ufs/experiment/global-workflow.${IJ_SUFFIX}
+export IJ_UFS_TAG=9193010aa52f17a7b3618fd75b4e3c03a79c3c59
 export SLURM_ACCOUNT=epic
 
 cd /scratch3/NCEPDEV/stmp/Benjamin.Koziol/sandbox/NOAA-EPIC/jedi-ufs-scripting/in-line
-
 source ./ij-do-all.sh
