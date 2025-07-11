0630bb3c4960253e97da8bb7b46bd269a67a824a
Ben Koziol <bkoziol@redlineperf.com>
2025-07-11 11:14:19 -0500

    build control

--- control/cntl-do-all.sh
+++ control/cntl-do-all.sh
@@ -3,8 +3,8 @@
 
 set -xue -o pipefail
 
-_CNTL_CLONE_DIR=/scratch3/NCEPDEV/stmp/Benjamin.Koziol/inline-jedi-ufs/control/global-workflow.${_CNTL_SUFFIX}
 _CNTL_SUFFIX=20250711-1056
+_CNTL_CLONE_DIR=/scratch3/NCEPDEV/stmp/Benjamin.Koziol/inline-jedi-ufs/control/global-workflow.${_CNTL_SUFFIX}
 _WD=/scratch3/NCEPDEV/stmp/Benjamin.Koziol/sandbox/NOAA-EPIC/jedi-ufs-scripting/control
 
 cd ${_WD}
