fc0147ece1656c5563a0e667fcdb37e692bfbb5f
Ben Koziol <bkoziol@redlineperf.com>
2025-07-14 15:10:11 -0500

    build te gw

--- control/work/benkozi-travis-elless-gw-.sh
+++ control/work/benkozi-travis-elless-gw-.sh
@@ -7,6 +7,6 @@ _SUFFIX=20250714-1506
 
 cd /scratch3/NCEPDEV/stmp/Benjamin.Koziol/inline-jedi-ufs
 mkdir control-jedi_enkf_only
-cd control-jedi-enkf_only
+cd control-jedi_enkf_only
 
 git clone -b feature/jedi_enkf_only --jobs 8 https://github.com/TravisElless-NOAA/global-workflow.git global-workflow.${_SUFFIX}
