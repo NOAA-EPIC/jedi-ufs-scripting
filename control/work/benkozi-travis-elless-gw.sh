255c7e95e76726b52a2cf13d884fd25eeaad9421
Ben Koziol <bkoziol@redlineperf.com>
2025-07-14 17:09:16 -0500

    build te gw

--- control/work/benkozi-travis-elless-gw.sh
+++ control/work/benkozi-travis-elless-gw.sh
@@ -10,6 +10,8 @@ _UFS_HASH=9193010aa52f17a7b3618fd75b4e3c03a79c3c59
 
 #mkdir -p ${_CLONE_DIR}
 #git clone -b feature/jedi_enkf_only --jobs 8 --recursive https://github.com/TravisElless-NOAA/global-workflow.git ${_CLONE_DIR}
+#cd ${_CLONE_DIR}
+#git apply ./travis-elless.patch
 
 #cd ${_CLONE_DIR}/sorc/ufs_model.fd
 #git fetch origin develop
@@ -19,5 +21,5 @@ _UFS_HASH=9193010aa52f17a7b3618fd75b4e3c03a79c3c59
 # Using build_all.sh from https://github.com/TravisElless-NOAA/global-workflow/blob/develop/sorc/build_all.sh
 cd ${_CLONE_DIR}/sorc
 echo "INFO: sorc build directory= $(readlink -f .)"
-./build_all.sh -a ATM all 2>&1 | tee out.build_all.sh.${_SUFFIX}
+./build_all.sh -a ATM -gu 2>&1 | tee out.build_all.sh.${_SUFFIX}
 ./link_workflow.sh | tee out.link_workflow.sh.${_SUFFIX}
