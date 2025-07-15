6e1c710fc463c38b56d66037edd1c215f65a8397
Ben Koziol <bkoziol@redlineperf.com>
2025-07-15 15:29:09 -0500

    build te gw

--- in-line/ij-experiment.sh
+++ in-line/ij-experiment.sh
@@ -13,17 +13,17 @@ source ./util/ij-load-modules.sh
 # C's experiment configuration: /scratch2/BMC/gsienkf/cctong/exp_psonly_4mem/exp/exp_psonly_4mem
 
 cd ${IJ_CLONE_DIR}/dev/workflow
-./setup_expt.py gfs forecast-only \
+./setup_expt.py gfs cycled \
   --app ATM \
   --idate 2022010312 \
   --edate 2022010400 \
   --resdetatmos 48 \
   --pslot test_control \
-  --configdir ${IJ_CLONE_DIR}/dev/parm/config \
+  --configdir ${IJ_CLONE_DIR}/dev/parm/config/gfs \
   --comroot ${_WD}/comroot \
-  --expdir ${_WD}/exp
-#  --resensatmos 96 \
-#  --nens 4 \
+  --expdir ${_WD}/exp \
+  --resensatmos 48 \
+  --nens 4
 #  --icsdir /work/noaa/da/telless/data/ICs/2022010312_v17_C192C96_output_new/
 
 ./setup_xml.py ${_WD}/exp/test_control
