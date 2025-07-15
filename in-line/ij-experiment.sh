57fd1c9e4ba4dc356bd00dc7c228aa1056d7b86a
Ben Koziol <bkoziol@redlineperf.com>
2025-07-15 15:01:17 -0500

    build te gw

--- in-line/ij-experiment.sh
+++ in-line/ij-experiment.sh
@@ -4,13 +4,15 @@
 set -xue -o pipefail
 
 export ACCOUNT=epic
-
 _WD=/scratch3/NCEPDEV/stmp/Benjamin.Koziol/inline-jedi-ufs/control
-_GWD=${_WD}/global-workflow.20250711-1056
+export IJ_CLONE_DIR=${_WD}/global-workflow.20250711-1056
+
+source ./util/ij-env.sh
+source ./util/ij-load-modules.sh
 
 # C's experiment configuration: /scratch2/BMC/gsienkf/cctong/exp_psonly_4mem/exp/exp_psonly_4mem
 
-cd ${_GWD}/dev/workflow
+cd ${IJ_CLONE_DIR}/dev/workflow
 ./setup_expt.py gfs forecast-only \
   --app ATM \
   --idate 2022010312 \
@@ -19,7 +21,7 @@ cd ${_GWD}/dev/workflow
   --resensatmos 96 \
   --nens 4 \
   --pslot test_control \
-  --configdir ${_GWD}/dev/parm/config \
+  --configdir ${IJ_CLONE_DIR}/dev/parm/config \
   --comroot ${_WD}/comroot \
   --expdir ${_WD}/exp
 #  --icsdir /work/noaa/da/telless/data/ICs/2022010312_v17_C192C96_output_new/
