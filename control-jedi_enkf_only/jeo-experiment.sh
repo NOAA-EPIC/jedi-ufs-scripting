a93183dcc162acfb9064a84ab00ead9cd3b3ab31
Ben Koziol <bkoziol@redlineperf.com>
2025-07-17 08:59:13 -0500

    moved files

---
+++ control-jedi_enkf_only/jeo-experiment.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+set -xue -o pipefail
+
+export HPC_ACCOUNT=epic
+_WD=/scratch3/NCEPDEV/stmp/Benjamin.Koziol/inline-jedi-ufs/experiment
+export GW_CLONE_DIR=${_WD}/global-workflow.20250711-1056
+
+source ../util/env.sh
+source ../util/load-modules.sh
+
+# C's experiment configuration: /scratch2/BMC/gsienkf/cctong/exp_psonly_4mem/exp/exp_psonly_4mem
+
+cd ${GW_CLONE_DIR}/dev/workflow
+./setup_expt.py gfs cycled \
+  --app ATM \
+  --idate 2022010312 \
+  --edate 2022010400 \
+  --resdetatmos 192 \
+  --pslot test_control \
+  --configdir ${GW_CLONE_DIR}/dev/parm/config/gfs \
+  --comroot ${_WD}/comroot \
+  --expdir ${_WD}/exp \
+  --resensatmos 96 \
+  --nens 4 \
+  --icsdir /scratch2/BMC/gsienkf/cctong/ICs/2022010312_v17_C192C96_output_new/
+#  --icsdir /work/noaa/da/telless/data/ICs/2022010312_v17_C192C96_output_new/
+
+./setup_xml.py ${_WD}/exp/test_control
