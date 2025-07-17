3f698f63a8d4d46692ae3d7fdb190fafce35b105
Ben Koziol <bkoziol@redlineperf.com>
2025-07-17 15:35:17 -0500

    build new control

--- control/cntl-do-all.sh
+++ control/cntl-do-all.sh
@@ -11,14 +11,14 @@ _CNTL_HASH=ff5476b
 
 #cd ${_WD}
 #git clone -b ${_CNTL_BRANCH} --recursive --jobs 8 https://github.com/NOAA-EMC/global-workflow "${_CNTL_CLONE_DIR}" 2>&1 | tee out.clone.${_CNTL_SUFFIX}
-cd ${_CNTL_CLONE_DIR}
-git checkout ${_CNTL_HASH}
-git submodule update --init --recursive --jobs 8
+#cd ${_CNTL_CLONE_DIR}
+#git checkout ${_CNTL_HASH}
+#git submodule update --init --recursive --jobs 8
 
 #cd "${_CNTL_CLONE_DIR}"
 #readlink -f .
 #ls -lht
 #git apply ${_WD}/build_ufs.patch
 
-#cd "${_CNTL_CLONE_DIR}"/sorc
-#./build_all.sh -a ATM all 2>&1 | tee "${_WD}"/out.build."${_CNTL_SUFFIX}"
+cd "${_CNTL_CLONE_DIR}"/sorc
+./build_all.sh -a ATM all 2>&1 | tee "${_WD}"/out.build."${_CNTL_SUFFIX}"
