f3c6308a63e46fb0ee9146e01b43f9fa28f329f0
Ben Koziol <bkoziol@redlineperf.com>
2025-07-22 14:03:24 -0500

    build experiment

--- in-line/work/benkozi-work.sh
+++ in-line/work/benkozi-work.sh
@@ -2,7 +2,7 @@
 
 set -xue -o pipefail
 
-export IJ_SUFFIX=20250711-1056
+export IJ_SUFFIX=20250722-1400
 export GW_CLONE_DIR=/scratch3/NCEPDEV/stmp/Benjamin.Koziol/inline-jedi-ufs/experiment/global-workflow.${IJ_SUFFIX}
 export IJ_UFS_TAG=9193010aa52f17a7b3618fd75b4e3c03a79c3c59
 export SLURM_ACCOUNT=epic
@@ -11,6 +11,6 @@ cd /scratch3/NCEPDEV/stmp/Benjamin.Koziol/sandbox/NOAA-EPIC/jedi-ufs-scripting/i
 
 source ../util/env.sh
 
-#source ./ij-clone.sh 2>&1 | tee out.ij-clone."${IJ_SUFFIX}"
-#source ./ij-build.sh 2>&1 | tee out.ij-build."${IJ_SUFFIX}"
-source ./ij-test.sh 2>&1 | tee out.ij-test."${IJ_SUFFIX}"
+source ../ij-clone.sh 2>&1 | tee out.ij-clone."${IJ_SUFFIX}"
+source ../ij-build.sh 2>&1 | tee out.ij-build."${IJ_SUFFIX}"
+source ../ij-test.sh 2>&1 | tee out.ij-test."${IJ_SUFFIX}"
