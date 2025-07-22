abb8141555c65d30402aaa06cbd6ac40907d315b
Ben Koziol <bkoziol@redlineperf.com>
2025-07-22 14:08:56 -0500

    build experiment

--- in-line/work/benkozi-work.sh
+++ in-line/work/benkozi-work.sh
@@ -7,8 +7,6 @@ export GW_CLONE_DIR=/scratch3/NCEPDEV/stmp/Benjamin.Koziol/inline-jedi-ufs/exper
 export IJ_UFS_TAG=9193010aa52f17a7b3618fd75b4e3c03a79c3c59
 export SLURM_ACCOUNT=epic
 
-cd /scratch3/NCEPDEV/stmp/Benjamin.Koziol/sandbox/NOAA-EPIC/jedi-ufs-scripting/in-line
-
 source ../util/env.sh
 
 source ../ij-clone.sh 2>&1 | tee out.ij-clone."${IJ_SUFFIX}"
